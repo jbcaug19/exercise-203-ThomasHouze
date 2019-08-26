561596749591310c656f5201ca8de2b848111788
ThomasHouze <mrbugsbunny1st@gmail.com>
2019-08-26 16:21:42 -0400

    exercise 2.03 from wk 26

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+public class Song {
+    private long id;
+    private String fname;
+    private String lname;
+    private int ssn;
+    private int dob;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+}
+
